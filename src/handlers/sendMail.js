ef73ba4002953c48bb91f7f9faa2649bf860e856
Muntasir Biojid <muntasir2165@hotmail.com>
2023-02-17 19:45:43 -0500

    61-Manually Sending SQS Messages

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from "aws-sdk";
 const ses = new AWS.SES({ region: "eu-west-1" });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log("record processing", record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: "muntasir2165@hotmail.com",
     Destination: {
-      ToAddresses: ["muntasir2165@gmail.com"],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: "Hello from Codingly!",
+          Data: body,
         },
       },
       Subject: {
-        Data: "Test Mail",
+        Data: subject,
       },
     },
   };
